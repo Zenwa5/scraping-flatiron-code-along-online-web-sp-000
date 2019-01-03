0d4ec3714dfd36d3dbac200c0fdfd1cf36c82ea1
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-01-03 02:01:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,9 +12,9 @@ class Scraper
          course = Course.new
          course.title = post.css("h2").text
          course.schedule = post.css(".date").text
-         course.description = post.css("p").text
-         
-     end
+         course.description = post.css("p").text     
+      end
+     
   end
 end
 
